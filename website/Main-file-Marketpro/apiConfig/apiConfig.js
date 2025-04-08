7521cbe56bba03c54eb295fa61f2cbd39a671479
TechSolutionor98 <techsolutionor1@gmail.com>
2025-04-08 10:13:26 +0500

    remove Inner admin code
    remove Inner admin code

--- website/Main-file-Marketpro/apiConfig/apiConfig.js
+++ website/Main-file-Marketpro/apiConfig/apiConfig.js
@@ -1,9 +1,5 @@
-// const API_CUSTOMER_URL =  "https://server.techatooz.com"
-// const API_CUSTOMER_URL = 'http://localhost:5000';
-// const API_ADMINGRAB_URL =  "http://localhost:3000"
+const API_CUSTOMER_URL = 'http://localhost:5000';
+const API_ADMINGRAB_URL =  "http://localhost:3000"
 
-// https://admingrab.techatooz.com/
-// https://server.techatooz.com/
-
-const API_ADMINGRAB_URL = "https://admingrab.techatooz.com"
-const API_CUSTOMER_URL = "https://server.techatooz.com"
+// const API_ADMINGRAB_URL = "https://admingrab.techatooz.com"
+// const API_CUSTOMER_URL = "https://server.techatooz.com"
