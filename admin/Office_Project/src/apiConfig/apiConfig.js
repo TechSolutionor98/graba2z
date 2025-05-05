ac0cdcbaa53d50d2993edf61797255cf9429d873
TechSolutionor98 <techsolutionor1@gmail.com>
2025-05-05 19:18:40 +0500

    set admin frontend railway urls
    set admin frontend railway urls

--- admin/Office_Project/src/apiConfig/apiConfig.js
+++ admin/Office_Project/src/apiConfig/apiConfig.js
@@ -1,4 +1,6 @@
 // const API_ADMINGRAB_URL =  "http://localhost:3000"
 // const API_CUSTOMER_URL = 'http://localhost:5000';
-const API_ADMINGRAB_URL = "https://admingrab.techatooz.com"
-const API_CUSTOMER_URL = "https://server.techatooz.com"
+// const API_ADMINGRAB_URL = "https://admingrab.techatooz.com"
+// const API_CUSTOMER_URL = "https://server.techatooz.com"
+const API_ADMINGRAB_URL = "https://graba2z-admin-backend-production.up.railway"
+const API_CUSTOMER_URL = "https://graba2z-backend-production.up.railway.app"
